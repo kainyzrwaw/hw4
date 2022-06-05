d4c526e580fe65029050b370e3fda61841371dba
kainyzrwaw <sydney.wawrzyniak@kellogg.northwestern.edu>
2022-06-05 22:12:04 +0000

    Authorization for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,7 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
+    @place = Place.find_by({ "id" => params["id"]})
     @posts = Post.where({ "place_id" => @place["id"], "user_id" => session["user_id"]})
   end
 
