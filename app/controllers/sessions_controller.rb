1a272223a9566d22690ca03c8a2964f485bd72f4
kainyzrwaw <sydney.wawrzyniak@kellogg.northwestern.edu>
2022-06-05 16:24:03 +0000

    Cookies complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by({ "email" => params["email"] })
     if @user
-      if @user["password"] == params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # assign a cookie
+      session["user_id"] = @user["id"]
         # login the user
         flash["notice"] = "You've logged in."
         redirect_to "/places"
